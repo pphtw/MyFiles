ad58c696ca44cf958d7bae3a1b724808cd1e2c9f
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2022-10-26 08:35:43 +0700

    js update

---
+++ Practice/MyPractice/Money.js
@@ -0,0 +1,21 @@
+function calculateYears(principal, interest, tax, desired) {
+    let sum = principal
+    let year = 0
+
+    if (principal === desired) return 0
+
+    do{
+        i = (sum * interest)
+        t = i * tax
+        sum = sum + i - t
+        year++
+    }
+    while(sum <= desired)
+
+    return year
+}
+
+console.log(calculateYears(1000, 0.05, 0.18, 1100)); //3
+console.log(calculateYears(1000,0.01625,0.18,1200)); // 14
+console.log(calculateYears(1000,0.05,0.18,1000)); // 0
+
--- Practice/MyPractice/upArray.js
+++ Practice/MyPractice/upArray.js
@@ -2,7 +2,7 @@ function upArray(arr){
     arr.filter((e) => e !== null)
     if(arr.filter((e) => e < 0).length > 0 || arr.length === 0 || arr.filter((e) => e > 9).length > 0) return null;
 
-    if(arr[0] === 0){
+    if(arr[0] === 0 || arr[arr.length -1] === 0){
         let last = arr[arr.length-1] + 1;
         return arr.map((e,i) => i === arr.length-1? e+1: e);
     }
@@ -20,3 +20,4 @@ console.log(upArray([1,-9])) //null
 console.log(upArray([1,10])) // [1,1,1]
 console.log(upArray([28])) // [2,9]
 console.log(upArray([1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]));
+console.log(upArray([0,1,9,0,3,0]));
