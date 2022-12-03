0ec78315799ec9e484038b65438e14d0db1b3d53
pphtw <phutawan.palakavong@gmail.kmutt.ac.th>
2022-12-03 12:19:54 +0700

    retest

---
+++ retestModule2.js
@@ -0,0 +1,45 @@
+// const fn  = x => x
+// console.log(fn('hello, world'))
+
+// function doSomething(x){
+//     x[1] = 5
+// }
+// const arr = [1,2,3]
+// doSomething(arr)
+// console.log(arr);
+
+// const obj = {
+//     x:1,
+//     y:2,
+//     xy(){
+//         return this.x * this.y
+//     }
+// }
+
+// console.log(obj.xy()) //รันได้
+
+// class Circle {
+//     constructor (raduis){
+//         this.raduis = raduis;
+//     }
+// }
+// const obj1 = new Circle(2.5)
+// const obj2 = Object.create(obj1)
+// console.log(obj2.raduis)
+
+// const obj1 = {x:'M', y:35}
+// const obj2 = {x:'XL', z:28}
+// const obj3 = {...obj1, ...obj2}
+
+// console.log(obj3)
+
+// const [a] = [3,6,9]
+// console.log(a)
+
+// const {no, author} = {id: 124, author: 'Suda DeeJai'}
+// console.log(no)
+// console.log(author)
+
+const student = {id: 66, name: 'Phutawan'}
+student.id = 100
+console.log(student)
